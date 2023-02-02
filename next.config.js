ccc29b7a887cd56a6fabf9d613b8e3af96044cdb
Sujal Ranjan <90925047+Sranjan0208@users.noreply.github.com>
2023-02-03 02:02:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,5 @@ module.exports = {
     domains: ["links.papareact.com"],
   },
   // Only add public key to next.config.js, Not the private key
-  env: {
-    mapbox_key:
-      "pk.eyJ1Ijoic3JhbmphbjAyMDgiLCJhIjoiY2xkbHhyOXFoMDQwaTNubXgwOTQyeW42ayJ9.t0Jr84A6n0dS2DFRvFDHKQ",
-  },
+  
 };
